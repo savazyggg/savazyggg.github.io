172243119d373eec287060d40cd374c3d01e8781
younji <122354302+savazyggg@users.noreply.github.com>
2023-05-12 04:40:07 +0900

    Add files via upload

---
+++ portfoilio/commands.js
@@ -0,0 +1,104 @@
+var youtube = "";
+var twitter = "";
+var password = "";
+var notion = "younjilee.notion.site";
+var velog = "https://velog.io/@savazy_gg";
+var github = "https://github.com/savazyggg";
+var email = "mailto:younji0715@gmail.com";
+const portfolio =
+  "https://blossom-caboc-406.notion.site/747aef2417084f708e8e8db6e69a2347";
+
+whois = [
+  "<br>",
+  "Hey, I'm Forrest!👋",
+  "I'm a software developer and content creator, who builds engaging websites like this one",
+  "and makes YouTube videos about computer science & software engineering.",
+  "After graduating with a Bachelor's in Computer Science, I worked professionally",
+  "as a software engineer building enterprise web applications for Fortune 500 companies.",
+  "While doing all of that, I documentned my coding journey on YouTube - trying to enlighten",
+  "the next generation of developers and help them navigate the crazy world that is software",
+  "development & computer science.",
+  "Before I knew it, that online presence took on a life of its own, to the point where I knew",
+  "I needed to make the jump from software engineering to full time content creator, and it's",
+  "the best decision I ever made.",
+  "Now, I make videos about creating cool shit like this terminal website, and hosting my",
+  "podcast 'Decoded w/ Forrest Knight.' What most people don't know, and will only know",
+  "because they're reading this right now, is that I also run a creative & media agency.",
+  "We partner with clients to drive their business outcomes using modern marketing strategies.",
+  "<br>",
+  "To see more detail, run:",
+  '<span class="command">npm run start</span>',
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'notion         <a href="' +
+    notion +
+    '" target="_blank">notion/younji' +
+    "</a>",
+  'velog          <a href="' +
+    velog +
+    '" target="_blank">velog/savazy_gg' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="_blank">github/savazyggg' +
+    "</a>",
+  "<br>",
+];
+
+//포트폴리오 url
+projects = [
+  "<br>",
+  "Still curating... most projects are on GitHub, or confidential.",
+  "But still want to see more,",
+  "<br>",
+  "To see more detail, run:",
+  '<span class="command">npm run start</span>',
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Younji Lee?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           For a list of available commands',
+  '<span class="command">email</span>          email me. younji0715@gmail.com',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">younji Lee (YJ) Not A Corporation. All knights reserved.</span>',
+  "                                                                                                                                                        ",
+  "                                                                                                                                                        ",
+  "                                                                           jjjj   iiii                    LLLLLLLLLLL                                                     ",
+  "                                                                          j::::j i::::i                   L:::::::::L                                                     ",
+  "                                                                           jjjj   iiii                    L:::::::::L                                                     ",
+  "                                                                                                          LL:::::::LL                                                     ",
+  "yyyyyyy           yyyyyyy ooooooooooo   uuuuuu    uuuuuunnnn  nnnnnnnn   jjjjjjjiiiiiii                     L:::::L              ",
+  " y:::::y         y:::::yoo:::::::::::oo u::::u    u::::un:::nn::::::::nn j:::::ji:::::i                     L:::::L                ",
+  "  y:::::y       y:::::yo:::::::::::::::ou::::u    u::::un::::::::::::::nn j::::j i::::i                     L:::::L               ",
+  "   y:::::y     y:::::y o:::::ooooo:::::ou::::u    u::::unn:::::::::::::::nj::::j i::::i                     L:::::L              ",
+  "    y:::::y   y:::::y  o::::o     o::::ou::::u    u::::u  n:::::nnnn:::::nj::::j i::::i                     L:::::L              ",
+  "     y:::::y y:::::y   o::::o     o::::ou::::u    u::::u  n::::n    n::::nj::::j i::::i                     L:::::L               ",
+  "      y:::::y:::::y    o::::o     o::::ou::::u    u::::u  n::::n    n::::nj::::j i::::i                     L:::::L                ",
+  "       y:::::::::y     o::::o     o::::ou:::::uuuu:::::u  n::::n    n::::nj::::j i::::i                     L:::::L         LLLLLL   ",
+  "        y:::::::y      o:::::ooooo:::::ou:::::::::::::::uun::::n    n::::nj::::ji::::::i     ::::::::     LL:::::::LLLLLLLLL:::::L   ",
+  "         y:::::y       o:::::::::::::::o u:::::::::::::::un::::n    n::::nj::::ji::::::i     ::::::::     L::::::::::::::::::::::L  ",
+  "        y:::::y         oo:::::::::::oo   uu::::::::uu:::un::::n    n::::nj::::ji::::::i     ::::::::     L::::::::::::::::::::::L    ",
+  "       y:::::y            ooooooooooo       uuuuuuuu  uuuunnnnnn    nnnnnnj::::jiiiiiiii     ::::::::     LLLLLLLLLLLLLLLLLLLLLLLL    ",
+  "      y:::::y                                                             j::::j                                                                        ",
+  "     y:::::y                                                    jjjj      j::::j                                                                        ",
+  "    y:::::y                                                    j::::jj   j:::::j    2023 -v                                                     ",
+  "   y:::::y                                                     j::::::jjj::::::j                                                                        ",
+  "  yyyyyyy                                                       jj::::::::::::j                                                                         ",
+  "                                                                  jjj::::::jjj                                                                          ",
+  "                                                                     jjjjjj                                                                             ",
+  '<span class="color2">Welcome to interactive web terminal.</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+];
---
+++ portfoilio/main.js
@@ -0,0 +1,171 @@
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer");
+var textarea = document.getElementById("texter");
+var terminal = document.getElementById("terminal");
+
+var git = 0;
+let pwd = false;
+var commands = [];
+
+setTimeout(function () {
+  loopLines(banner, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+
+console.log(
+  "%cyou are interedted in ME!😁",
+  "color: #04ff00; font-weight: bold; font-size: 24px;"
+);
+
+//init
+textarea.value = "";
+command.innerHTML = textarea.value;
+pw = textarea.value;
+
+function enterKey(e) {
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+  }
+  if (pw) {
+    let et = "*";
+    let w = textarea.value.length;
+    command.innerHTML = et.repeat(w);
+    if (textarea.value === password) {
+      pwd = true;
+    }
+    if (pwd && e.keyCode == 13) {
+      loopLines(secret, "color2 margin", 120);
+      command.innerHTML = "";
+      textarea.value = "";
+      pwd = false;
+      pw = false;
+      liner.classList.remove("password");
+    } else if (e.keyCode == 13) {
+      addLine("Wrong password", "error", 0);
+      command.innerHTML = "";
+      textarea.value = "";
+      pw = false;
+      liner.classList.remove("password");
+    }
+  } else {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine("visitor@Visiors.com:~$ " + command.innerHTML, "no-animation", 0);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+}
+
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "help":
+      loopLines(help, "color2 margin", 80);
+      break;
+    case "whois":
+      loopLines(whois, "color2 margin", 80);
+      break;
+    case "sudo":
+      addLine("Oh no, you're not admin...", "color2", 80);
+      break;
+    case "social":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "projects":
+      loopLines(projects, "color2 margin", 80);
+      break;
+    case "history":
+      addLine("<br>", "", 0);
+      loopLines(commands, "color2", 80);
+      addLine("<br>", "command", 80 * commands.length + 50);
+      break;
+    case "email":
+      addLine("younji0715@gamil.com", "color2", 80);
+      break;
+    case "clear":
+      setTimeout(function () {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "banner":
+      loopLines(banner, "", 80);
+      break;
+    // socials
+    case "notion":
+      addLine("Opening Notion...", "color2", 0);
+      newTab(notion);
+      break;
+    case "velog":
+      addLine("Opening Velog...", "color2", 0);
+      newTab(velog);
+      break;
+    case "github":
+      addLine("Opening GitHub...", "color2", 0);
+      newTab(github);
+      break;
+    case "npm run start":
+      addLine("Opening Portfolio...", "color2", 0);
+      newTab(portfolio);
+      break;
+    default:
+      addLine(
+        '<span class="inherit">Command not found. For a list of commands, type <span class="command">\'help\'</span>.</span>',
+        "error",
+        100
+      );
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function () {
+    window.open(link, "_blank");
+  }, 1500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function () {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function (item, index) {
+    addLine(item, style, index * time);
+  });
+}
